b3a480f642483bae6d060590f6490a3cb67d747d
Edmund Haselwanter <e.haselwanter@cloudbau.de>
2012-11-21 16:08:07 +0100

    add razor default tftp stuff

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,11 @@
 name             "razor"
-maintainer       "YOUR_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
+maintainer       "Edmund Haselwanter"
+maintainer_email "e.haselwanter@cloudbau.de"
+license          "Apache 2.0"
 description      "Installs/Configures razor"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
+
+depends "apt"
+
 
